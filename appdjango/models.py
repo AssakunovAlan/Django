06d3fe9bb020bfc68c4eb94bda4138f09547ab33
codif <alan.assakunov@gmail.com>
2025-04-06 16:13:41 +0300

    irgjier

--- appdjango/models.py
+++ appdjango/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
+
+
+class Author(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Автор')
+    birthday = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
-    title = models.CharField(max_length=100) #Принимает строку
-    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=100, verbose_name='Название')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
     publication_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_date})"
 
